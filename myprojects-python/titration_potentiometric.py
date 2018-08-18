587b068c8b029556377c9a6671e11e09db72397c
Arpan Ghosh <thisisarpanghosh@gmail.com>
2018-08-18 14:30:56 +0530

    potentiometric titration

---
+++ myprojects-python/titration_potentiometric.py
@@ -0,0 +1,137 @@
+
+# coding: utf-8
+
+# In[1]:
+
+import matplotlib.pyplot as plt
+import numpy as np
+import sympy as sp
+from sympy.abc import x
+
+
+
+# In[2]:
+
+acid_C = 0.1
+base_C = 0.1
+acid_vol = 25
+const = 10**-7
+
+a = acid_C*acid_vol
+b = base_C
+c = acid_vol
+z = const
+
+
+# In[6]:
+
+def get_ph(base_vol):
+    try:
+        f = 14+sp.log(((b*x-a)/(c+x))+z,10)
+        result = f.evalf(subs={x:base_vol})
+        float(result)
+        return result
+        
+    except:
+        f = -sp.log(((a-b*x)/(c+x))+z,10)
+        result = f.evalf(subs={x:base_vol})
+        float(result)
+        return result
+
+def get_diff_ph(base_vol):
+    try:
+        if get_ph(base_vol) < 7:
+            raise ValueError("Error")
+        f = 14+sp.log(((b*x-a)/(c+x))+z,10)
+        y = sp.diff(f,x)
+        result = y.evalf(subs={x:base_vol})
+        result = float(result)
+        return result
+        
+    except:
+        f = -sp.log(((a-b*x)/(c+x))+z,10)
+        y = sp.diff(f,x)
+        result = y.evalf(subs={x:base_vol})
+        result = float(result)
+        return result
+    
+def get_diff_2_ph(base_vol):
+    try:
+        if get_ph(base_vol) < 7:
+            raise ValueError("Error")
+        f = 14+sp.log(((b*x-a)/(c+x))+z,10)
+        y = sp.diff(f,x,2)
+        result = y.evalf(subs={x:base_vol})
+        return result
+        
+    except:
+        f = -sp.log(((a-b*x)/(c+x))+z,10)
+        y = sp.diff(f,x,2)
+        result = y.evalf(subs={x:base_vol})
+        float(result)
+        return result
+
+def get_emf(ph):
+    return 0.24+0.059*ph
+
+def get_diff_emf(d_ph):
+    return 0.059*d_ph
+
+
+# In[7]:
+
+base_vol_list = np.linspace(0,50,1000)
+ph_list =  map(get_ph,base_vol_list)
+ph_diff_list_VOL = map(get_diff_ph,base_vol_list)
+ph_diff_2_list_VOL = map(get_diff_2_ph,base_vol_list)
+emf_list = map(get_emf,ph_list)
+emf_diff_list_VOL = map(get_diff_emf,ph_diff_list_VOL)
+
+
+# In[18]:
+
+plt.plot(base_vol_list,ph_diff_list_VOL)
+plt.grid()
+
+
+# In[19]:
+
+plt.plot(base_vol_list,ph_list)
+plt.grid()
+
+
+# In[20]:
+
+plt.plot(ph_list,emf_list)
+plt.grid()
+
+
+# In[21]:
+
+plt.plot(base_vol_list,emf_list)
+plt.grid()
+
+
+# In[22]:
+
+plt.plot(base_vol_list,emf_diff_list_VOL)
+plt.grid()
+
+
+# In[8]:
+
+plt.plot(base_vol_list,ph_diff_2_list_VOL)
+plt.grid()
+
+
+# In[9]:
+
+plt.plot(base_vol_list,ph_diff_list_VOL)
+plt.plot(base_vol_list,emf_diff_list_VOL)
+plt.grid()
+
+
+# In[ ]:
+
+
+
