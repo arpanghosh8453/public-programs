c90d4d1b73e2e7b2b3499298c511e13d517c7b9d
Arpan Ghosh <thisisarpanghosh@gmail.com>
2018-09-18 16:04:33 +0530

    Old files converted into Python 3

---
+++ myprojects-python_Python_3/myprojects-Python2_to_Python3/check_binary.py
@@ -0,0 +1,21 @@
+from __future__ import division
+import string 
+
+def istext(filename):
+    s=open(filename).read(512)
+    text_characters = "".join(map(chr, range(32, 127)) + list("\n\r\t\b"))
+    _null_trans = string.maketrans("", "")
+    if not s:
+        # Empty files are considered text
+        return True
+    if "\0" in s:
+        # Files with null bytes are likely binary
+        return False
+    # Get the non-text characters (maps a character to itself then
+    # use the 'remove' option to get rid of the text characters.)
+    t = s.translate(_null_trans, text_characters)
+    # If more than 30% non-text characters, then
+    # this is considered a binary file
+    if float(len(t))/float(len(s)) > 0.30:
+        return False
+    return True
