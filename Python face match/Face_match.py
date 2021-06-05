0b6207f3db958f8c4ec721e559a39760160eb2b2
Arpan Ghosh <thisisarpanghosh@gmail.com>
2021-06-05 08:13:59 +0530

    Lockdown mode added

--- Python face match/Face_match.py
+++ Python face match/Face_match.py
@@ -5,32 +5,57 @@ import time,os
 picture_of_me = face_recognition.load_image_file("/home/arpan/Pictures/Myself/arpan.jpg")
 my_face_encoding = face_recognition.face_encodings(picture_of_me)[0]
 
+none_count = 0
+intruder_count = 0
+camera_fail = 0
+
 while True:
 
     cap = cv2.VideoCapture(2)
     cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
     cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
-    ret, frame = cap.read()
+    return_value, frame = cap.read()
     cap.release()
     
-    if ret:
+    if return_value:
         cv2.imwrite('/home/arpan/Pictures/test.jpg',frame)
+        unknown_picture = face_recognition.load_image_file("/home/arpan/Pictures/test.jpg")
+        webcam_feed = face_recognition.face_encodings(unknown_picture)
+
+        i_am_present = False
+
+        for face in webcam_feed:
+            if face_recognition.compare_faces([my_face_encoding], face, tolerance=0.47)[0]:
+                print("You are there | ",face_recognition.face_distance([my_face_encoding], face))
+                i_am_present = True
+            else:
+                #os.system('notify-send "Intruder Found"')
+                print("Intruder Found! | ",face_recognition.face_distance([my_face_encoding], face))
+        
+
+                
+        if len(webcam_feed) == 0:
+            #os.system('notify-send "None Found"')
+            print("None found")
+            none_count += 1
+        else:
+            if not i_am_present:
+                intruder_count += 1
+            else:
+                none_count = 0
+                intruder_count = 0
+                camera_fail = 0
     else:
         print("Webcam image failed!")
-  
-    unknown_picture = face_recognition.load_image_file("/home/arpan/Pictures/test.jpg")
-    webcam_feed = face_recognition.face_encodings(unknown_picture)
+        camera_fail += 1
+        #os.system('notify-send "No webcam Found"')
 
-    for face in webcam_feed:
-        if face_recognition.compare_faces([my_face_encoding], face, tolerance=0.53)[0]:
-            print("You are there")
-        else:
-            os.system('notify-send "Intruder Found"')
-            print(face_recognition.face_distance([my_face_encoding], face))
-            
-    if len(webcam_feed) == 0:
-        #os.system('notify-send "None Found"')
-        print("None found")
+  
+
+    if ((none_count >= 15) or (intruder_count >= 3)) or camera_fail >= 3:
+        print("Lockdown initiated")
+        os.system('notify-send "Lockdown initiated!"')
+        
     
     time.sleep(10)
     
